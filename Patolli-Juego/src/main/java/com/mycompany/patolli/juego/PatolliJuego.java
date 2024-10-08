880caaafb8aac0b0dfe17ca3394c0177a846360e
Enrique Rodríguez Angulo <enrique.rodriguez246966@potros.itson.edu.mx>
2024-10-07 20:30:54 -0700

    Primero commit

---
+++ Patolli-Juego/src/main/java/com/mycompany/patolli/juego/PatolliJuego.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patolli.juego;
+
+/**
+ *
+ * @author Enrique Rodriguez
+ */
+public class PatolliJuego {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
